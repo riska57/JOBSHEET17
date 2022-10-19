40f9c2fb1e19b7b8ef130419e27d6192345c745d
jobsheet12 <kothim@gmail.com>
2022-10-19 16:27:38 +0700

    jobsheet17

---
+++ app/src/main/java/com/example/riskajob17/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.riskajob17
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tab = findViewById<TabLayout>(R.id.tabLayout)
+        var fragment : Fragment = TPFLFragment()
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+
+        tab.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when(tab!!.position) {
+                    0 -> fragment = TPFLFragment()
+                    1 -> fragment = TOFragment()
+                    2 -> fragment = PPLGFragment()
+                    3 -> fragment = KulinerFragment()
+                    4 -> fragment = BusanaFragment()
+                }
+
+                val fT = supportFragmentManager.beginTransaction()
+                fT.replace(R.id.frameLayout, fragment)
+                fT.commit()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        })
+    }
+}
